f95e1d2994269e284ddda831eb829bcf651d3877
Philipp Hahmann <55932959+philipphahmann@users.noreply.github.com>
2025-10-07 19:53:56 -0300

    Develop (#9)
    * refactor: ajustado o zip da aplicação
    
    * fix: ajustado source_dir
    
    * refactor: build package apply
    
    * feat: ajustado a lambda para ler do secret manager e ajustado documentação/esteira/infra
    
    * chore: atualizado package-lock.json

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,9 @@ output "lambda_authorizer_arn" {
   description = "ARN da função Lambda do autorizador para ser usado no API Gateway."
   value       = aws_lambda_function.authorizer.arn
 }
+
+variable "public_key_secret_name" {
+  description = "O nome do segredo no AWS Secrets Manager que contém a chave pública."
+  type        = string
+  default     = "soat/jwt-public-key" # Exemplo, troque pelo seu
+}
